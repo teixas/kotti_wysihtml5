95f2e72151480590470ba19eee5fdbaed20ea216
Nuno Teixeira <teixas@gmail.com>
2013-03-03 13:29:13 +0000

    Load wysihtml5 editor with defalut options

--- kotti_wysihtml5/__init__.py
+++ kotti_wysihtml5/__init__.py
@@ -2,6 +2,8 @@
 
 from fanstatic import Library
 
+from js.bootstrap_wysihtml5 import bootstrap_wysihtml5
+
 library = Library('kotti_wysihtml5', 'static')
 
 
@@ -14,3 +16,12 @@ def kotti_configure(settings):
 
 def includeme(config):
     config.scan("kotti_wysihtml5")
+    try:
+        # kotti >= 0.8
+        from js.deform import resource_mapping
+        edit_needed = resource_mapping['tinymce'] = \
+            [bootstrap_wysihtml5, ]
+    except ImportError:  # pragma: no cover
+        # kotti < 0.8
+        from kotti.static import edit_needed
+        edit_needed.add(bootstrap_wysihtml5)
